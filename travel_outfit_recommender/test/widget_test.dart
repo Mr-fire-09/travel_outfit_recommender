dc2fc30248bf8a7458c03c41fe7dc57fe2f2d834
Neeraj Verma <neerajsingh009424@gmail.com>
2025-06-03 00:40:17 +0530

    update  welcome screen

--- travel_outfit_recommender/test/widget_test.dart
+++ travel_outfit_recommender/test/widget_test.dart
@@ -7,24 +7,52 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:travel_outfit_recommender/main.dart';
+import 'package:travel_outfit_recommender/screen/welcome_screen.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Welcome screen test', (WidgetTester tester) async {
+    // Build our app and trigger a frame
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that the welcome text is present
+    expect(find.text('Welcome to Your\nSmart Wardrobe!'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Verify that the "Created by Neeraj" text is present
+    expect(find.text('Created by Neeraj'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that the Create button is present
+    expect(find.text('Create'), findsOneWidget);
+
+    // Test navigation to outfit form page
+    await tester.tap(find.text('Create'));
+    await tester.pumpAndSettle();
+
+    // Verify that we're on the outfit form page
+    expect(find.text('Outfit Recommender'), findsOneWidget);
+  });
+
+  testWidgets('Outfit form test', (WidgetTester tester) async {
+    // Build the form page directly
+    await tester.pumpWidget(const MaterialApp(home: OutfitFormPage()));
+
+    // Verify that form elements are present
+    expect(find.byIcon(Icons.place), findsOneWidget);
+    expect(find.text('Summer'), findsOneWidget);
+    expect(find.text('Beach'), findsOneWidget);
+    expect(find.text('Vacation'), findsOneWidget);
   });
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WelcomeScreen(), // Your welcome page widget
+    );
+  }
+}
+
+class WelcomePage {}
