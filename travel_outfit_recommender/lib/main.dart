1c0b9d532ba368154524b9bc70023baf55bbbc17
Neeraj Verma <neerajsingh009424@gmail.com>
2025-06-03 12:07:44 +0530

    add some new features to the travel outfit recommender app

--- travel_outfit_recommender/lib/main.dart
+++ travel_outfit_recommender/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travel_outfit_recommender/screen/welcome_screen.dart';
 import 'screens/welcome_screen.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,8 +17,22 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.teal),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Your Smart Wardrobe'),
           backgroundColor: Colors.teal,
+          title: AnimatedTextKit(
+            animatedTexts: [
+              TypewriterAnimatedText(
+                'Welcome to Your Smart Wardrobe',
+                textStyle: const TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                speed: const Duration(milliseconds: 100),
+              ),
+            ],
+            totalRepeatCount: 1,
+            displayFullTextOnTap: true,
+          ),
         ),
         body: const WelcomeScreen(),
         bottomNavigationBar: Container(
