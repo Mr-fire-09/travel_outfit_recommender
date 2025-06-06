7336599b7627ae61ed3cf553e892d0d4338cc19b
Neeraj Verma <neerajsingh009424@gmail.com>
2025-06-06 12:25:07 +0530

    new feature: Add outfit recommendation based on weather conditions

--- travel_outfit_recommender/lib/main.dart
+++ travel_outfit_recommender/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travel_outfit_recommender/screen/welcome_screen.dart';
-import 'screens/welcome_screen.dart';
+
 import 'package:animated_text_kit/animated_text_kit.dart';
 
 void main() {
